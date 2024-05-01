7034f7cbb883b7e6c287a71b3bc5f437c66f52d4
Adittt11 <146904866+Adittt11@users.noreply.github.com>
2024-05-02 01:30:03 +0800

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,15 @@
+package TUGAS1_PBO;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Order {
+
+
+    static void MenuOrder(){
+        System.out.println("======== MENU ORDER ========");
+        System.out.println("| 1. Lihat Restoran         ");
+        System.out.println("| 2. Logout                 ");
+        System.out.println("============================");
+    }
+}
