c336558d2c62ef934938f534393f18ad6bc84a5e
Adittt11 <146904866+Adittt11@users.noreply.github.com>
2024-05-02 23:27:47 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,41 +1,48 @@
 package TUGAS1_PBO;
 public class Login {
-    private String username_user,password_user,
-                  username_admin,password_admin;
+
+    private String username_admin,password_admin,
+            username_user,password_user;
 
     public Login(){
-        username_user = "pelanggan";
-        password_user = "pelangganpw";
-
         username_admin = "admin";
         password_admin = "admin";
-    }
 
+        username_user = "pelanggan";
+        password_user = "pelanggan";
+    }
     //setter
-    public void setUsername_user(String username_user){
-        this.username_user = username_user;
-    }
-    public void setPassword_user(String password_user){
-        this.password_user = password_user;
-    }
-    public void setUsername_admin(String username_admin){
+    public void setUsername_admin(String username_admin) {
         this.username_admin = username_admin;
     }
-    public void setPassword_admin(String password_admin){
+
+    public void setPassword_admin(String password_admin) {
         this.password_admin = password_admin;
     }
 
-    //getter
-    public String getUsername_user() {
-        return username_user;
+    public void setUsername_user(String username_user) {
+        this.username_user = username_user;
     }
-    public String getPassword_user() {
-        return password_user;
+
+    public void setPassword_user(String password_user) {
+        this.password_user = password_user;
     }
+
+    //Getter
     public String getUsername_admin() {
         return username_admin;
     }
+
     public String getPassword_admin() {
         return password_admin;
     }
+
+    public String getUsername_user() {
+        return username_user;
+    }
+
+    public String getPassword_user() {
+        return password_user;
+    }
 }
+
