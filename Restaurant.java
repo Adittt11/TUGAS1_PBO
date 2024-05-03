e3f4a257c46442709ccc80be0a22c8015c790d4e
Adittt11 <146904866+Adittt11@users.noreply.github.com>
2024-05-03 11:18:45 +0800

    Update Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -1,37 +1,42 @@
 package TUGAS1_PBO;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collection;
 
-public class Order {
-    private Restaurant restaurant;
-    private HashMap<Menu, Integer> menus;
-    private int distance;
-    public Order(Restaurant restaurant){
-        this.restaurant = restaurant;
-        this.menus = new HashMap<>();
-    }
-    public Restaurant getRestaurant(){return this.restaurant;}
-    public HashMap<Menu, Integer> getMenus(){return this.menus;}
-    public int getDistance(){return this.distance;}
-    public void setDistance(int distance){this.distance = distance;}
+public class Restaurant {
+    private int id;
+    private String nama;
+    private String alamat;
+    private ArrayList<Menu> menus;
 
-    public int getTotalPrice(){
-        int totalPrince = 0;
-        for (Menu menu : this.menus.keySet()){
-            totalPrince += menu.getHarga() * this.menus.get(menu);
-        }
-        totalPrince += this.distance * 1000;
-        return totalPrince;
+    public Restaurant(int id, String nama, String alamat){
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.menus = new ArrayList<>();
     }
-    public void addMenu(Menu menu, int quantity){
-        if(this.menus.containsKey(menu)){
-            int currentQuantity = this.menus.get(menu);
-            this.menus.put(menu, currentQuantity + quantity);
-        }else {
-            this.menus.put(menu, quantity);
-        }
+    public int getId() {
+        setId(id);
+        return id;
     }
-
+    public ArrayList<Menu> getMenus(){return menus;}
+    public void setMenus(Menu menu){this.menus.add(menu);}
+    public void setId(int id){this.id = id;}
+    public String getNama() {
+        setNama(nama);
+        return nama;
+    }
+    public void setNama(String nama){this.nama = nama;}
+    public String getAlamat(){
+        setAlamat(alamat);
+        return alamat;
+    }
+    public void setAlamat(String alamat){this.alamat = alamat;}
+    @Override
+    public String toString(){
+        return "ID Restaurant : "+ id +
+                "|| nama + " + nama + ' ' +
+                "|| alamat + " + alamat +' ';
+    }
 
 }
